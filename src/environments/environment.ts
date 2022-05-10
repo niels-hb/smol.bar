eb71bcda95cbfac8262d899b5567738c21fedc50
Niels Heidbrink <niels.heidbrink@gmail.com>
2022-05-10 19:46:04 +0000

    Added configuration for Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'smol-bar',
+    appId: '1:215234163289:web:ae512cc481a1c7d4463748',
+    storageBucket: 'smol-bar.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyDVzOE-H4HSRaURLrkS5bQun46eMoYFlkM',
+    authDomain: 'smol-bar.firebaseapp.com',
+    messagingSenderId: '215234163289',
+  },
   production: false
 };
 

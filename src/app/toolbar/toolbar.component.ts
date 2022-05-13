541f8cd8732cb2c9ca9f21d8e2570fa8062b136a
Niels Heidbrink <niels.heidbrink@gmail.com>
2022-05-13 19:27:22 +0000

    Added initial toolbar

---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css'],
+})
+export class ToolbarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

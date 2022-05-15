002bf85e6fb76dee3a77bb5485fefe7ef540149b
Niels Heidbrink <niels.heidbrink@gmail.com>
2022-05-15 11:51:39 +0200

    Added secured routes for /profile and /stats

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedInGuard } from './guards/logged-in/logged-in.guard';
 import { ForwarderComponent } from './pages/forwarder/forwarder.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { StatsComponent } from './pages/stats/stats.component';
 
 const routes: Routes = [
   {
@@ -11,6 +14,16 @@ const routes: Routes = [
   {
     path: 'r/:id',
     component: ForwarderComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [LoggedInGuard],
+  },
+  {
+    path: 'stats',
+    component: StatsComponent,
+    canActivate: [LoggedInGuard],
   },
 ];
 

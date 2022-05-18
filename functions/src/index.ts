6a120d1d61d51d9696f4d33ddd801261fb66584f
Niels Heidbrink <niels.heidbrink@gmail.com>
2022-05-18 19:03:15 +0000

    Added clearExpiredRedirects Cloud Function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,55 @@
 import * as functions from "firebase-functions";
+import admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const db = admin.firestore();
+
+exports.clearExpiredRedirects = functions
+    .region("europe-central2")
+    .pubsub
+    .schedule("every 1 hours")
+    .onRun((_) => {
+      functions.logger.info("Deleting expired documents...");
+
+      findExpiredDocuments()?.then((expiredDocuments) => {
+        functions.logger.info(
+            "Found " + expiredDocuments.size + " expired documents..."
+        );
+
+        expiredDocuments.forEach((doc) => {
+          functions.logger.debug(
+              "Deleting document: " + doc.id
+          );
+          db.collection("redirects").doc(doc.id).delete();
+          functions.logger.debug(
+              "Deleted document: " + doc.id
+          );
+        });
+
+        functions.logger.info("Deleted expired documents.");
+      });
+
+      return null;
+    });
+
+/**
+ * Returns an array of all expired documents in the redirects collection.
+ * @return { Promise<QuerySnapshot<DocumentData>> | null }
+ *         A list of document references for all expired documents.
+ */
+function findExpiredDocuments() {
+  try {
+    return db
+        .collection("redirects")
+        .where(
+            "expiresAt",
+            "<=",
+            admin.firestore.Timestamp.now()
+        )
+        .get();
+  } catch (e) {
+    functions.logger.error(e);
+
+    return null;
+  }
+}
